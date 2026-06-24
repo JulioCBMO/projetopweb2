061a96371a48d429ade113b5cc6bff1027134db0
FELIPE MACEDO <144855211+felipemcd@users.noreply.github.com>
2026-06-24 21:03:27 +0000

    feat: atualiza entidade Participante com UserDetails e adiciona busca por nome

--- src/main/java/pweb2/quizz/Repository/ParticipanteRepository.java
+++ src/main/java/pweb2/quizz/Repository/ParticipanteRepository.java
@@ -1,14 +1,10 @@
 package pweb2.quizz.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import pweb2.quizz.model.Participante;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pweb2.quizz.model.Participante;
-
 public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
-    Optional<Participante> findByNome(String nome);
-  
-
     
+    Optional<Participante> findByNome(String nome);
 }
--- src/main/java/pweb2/quizz/model/Participante.java
+++ src/main/java/pweb2/quizz/model/Participante.java
@@ -1,32 +1,102 @@
 package pweb2.quizz.model;
 
-import lombok.Data;
-import java.io.Serializable;
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-@Data
-public class Participante implements Serializable {
-    
+public class Participante implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String nome;
     
-    private String email;
+    private String senha;
+    private String role;
 
-    private boolean admin;
+    public Participante() {}
 
-    @ManyToMany
-    @JoinTable(name = "participante_corrida")
-    private List<Corrida> corridasFeitas;
+    public Participante(String nome, String senha, String role) {
+        this.nome = nome;
+        this.senha = senha;
+        this.role = role;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+   
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nome; 
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
